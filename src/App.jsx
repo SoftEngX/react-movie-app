53ce1d4a4722c1f714049e2f861f5ff7a89b0f59
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-02-12 02:24:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+const Card = () => {
+  return (
+    <>
+      <h2>Card</h2>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <h2>Hello world</h2>
+    </>
+  );
+};
+export default App;
