2e29c307e81e3ab3218322033efb8fe0b47779b2
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-02-12 03:28:48 +0600

    Download all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-
-const Card = () => {
-  return (
-    <>
-      <h2>Card</h2>
-    </>
-  )
-}
+import React from "react";
 
 const App = () => {
   return (
-    <>
-      <h2>Hello world</h2>
-    </>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        
+        <p>Search</p>
+      </div>
+    </main>
   );
 };
+
 export default App;
